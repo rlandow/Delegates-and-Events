72bd7a2a7c0eeec2693e260cf0db0589e36417db
rlandow <35896738+rlandow@users.noreply.github.com>
2018-04-06 00:05:39 -0400

    First commit

---
+++ DelegatesEvents/DelegatesEvents/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesEvents
+{
+    //https://www.youtube.com/watch?v=jQgwEsJISy0
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder(); //publisher
+
+            var mailServive = new MailSevice(); //subscriber
+
+            var messageService = new Messageservice(); //subscriber
+
+            videoEncoder.VideoEncoded += mailServive.OnVideoEncoded; //the pointer
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded; //the pointer
+
+            videoEncoder.Encode(video);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+    }
+
+    public class MailSevice 
+        {
+        public void OnVideoEncoded(object source, VideoEventArgs e)
+        {
+            Console.WriteLine("MailService: Email sent..." + e.Video.Title);
+        }
+
+    
+    }
+
+    public class Messageservice
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e)
+        {
+            Console.WriteLine("MessageService: Seneind a text msssage...." + e.Video.Title);
+        }
+    }
+        
+
+}
---
+++ DelegatesEvents/DelegatesEvents/VideoEncoder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegatesEvents
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+    public class VideoEncoder
+    {
+        //public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+        //or
+        //public event VideoEncodedEventHandler VideoEncoded;
+
+
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video....");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+            
+        }
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+        }
+    }
+}
